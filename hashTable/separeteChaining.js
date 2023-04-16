cce520493ae153b92e31d5c343ed079cbb76a9a0
pv-kiran <kiranpv903@gmail.com>
2023-04-16 12:57:26 +0530

    hashtable completion

--- hashTable/separeteChaining.js
+++ hashTable/separeteChaining.js
@@ -88,7 +88,7 @@ myHashTable.set('dni' , 'Bharath');
 myHashTable.set('aus' , 'Australia');
 myHashTable.set('eng' , 'England');
 myHashTable.set('nge' , 'Britain');
-
+myHashTable.set('aus' , 'Queens Land');
 myHashTable.set('sa' , 'South Africa');
 
 myHashTable.display();
@@ -136,95 +136,3 @@ myHashTable.display();
 
 
 
-// class HashTable {
-//    constructor(size) {
-//       this.table = new Array(size);
-//       this.size = size ;
-//    }
-
-//    hashFunction(key = '') {
-//       let total = 0 ;
-//       for(let i = 0 ; i < key.length ; i++) {
-//         total = total + key.charCodeAt(i);
-//       }
-//       return total % this.size;
-//    } 
-
-//    set(key,value) {
-//       let index = this.hashFunction(key);
-//     //   console.log(index)
-//     //   this.table[index] = value;
-
-//       let bucket = this.table[index];
-//     //   console.log(bucket)
-//       if(!bucket) {
-//          this.table[index] = [[key , value]];
-//       } else {
-//         let sameKeyIndex = bucket.find(item => item[0] === key)
-//         if(sameKeyIndex) {
-//             sameKeyIndex[1] = value;
-//         } else {
-//             bucket.push([key , value]);
-//         }
-//       }
-
-
-//    }
-
-//    get(key) {
-//         let index = this.hashFunction(key);
-//         let bucket = this.table[index];
-//         let keyValArr = bucket.find(item => item[0] === key);
-//         if(keyValArr) {
-//             return keyValArr[1];
-//         } else {
-//             return undefined;
-//         }
-//    }
-
-//    remove(key) {
-//      let index = this.hashFunction(key);
-//      let bucket = this.table[index];
-//      let keyValArr = bucket.find(item => item[0] === key);
-//      if(keyValArr) {
-//         bucket.splice(bucket.indexOf(keyValArr) , 1);
-//      } else {
-//          return undefined;
-//      }
-//    }
-
-//    display() {
-//       for(let i = 0 ; i < this.size ; i++) {
-//          if(this.table[i]) {
-//             console.log(`${i} --> ${this.table[i]}`);
-//          }
-//       }
-//    }
-
-// }
-
-
-
-// const table = new HashTable(50);
-// table.set('in' , 'INDIA');
-// table.set('ni' , 'Japan');
-
-
-// table.get('ni');
-// table.remove('in');
-
-
-
-// table.set('eng' , 'England');
-// table.set('nz' , 'New Zeland');
-// table.set('aus' , 'Australia');
-// table.set('sua' , 'Sudan');
-
-
-
-// console.log(table.get('aus'))
-
-// console.log(table.remove('aus'))
-
-
-// table.display();
